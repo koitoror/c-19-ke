49ae2069f134d4122d0f0dff15b3cba29c876123
Daniel Kamar <kamarster@gmail.com>
2020-04-09 15:21:00 +0300

    Implement challenge 1

--- src/estimator.py
+++ src/estimator.py
@@ -1,31 +1,31 @@
-def estimator(data):
-    print(data)
-    reportedCases = data[data]
-    currentlyInfected = reportedCases * 10
-    severeImpact = reportedCases * 50
-    # severeImpact = currentlyInfected * 5
-    # severeImpact.currentlyInfected = currentlyInfected
-    requestedTime = data.timeToElapse/3
-    # # infectionsByRequestedTime = currentlyInfected ** requestedTime,
-    # infectionsByRequestedTimeC : currentlyInfected ** requestedTime
-    # infectionsByRequestedTimeS : severeImpact ** requestedTime
-    infectionsByRequestedTime = ''
+from math import floor
 
-    
-    impact = {
-        currentlyInfected,
-    }
+def requestedDays(data):
+    if data['periodType'] == 'days':
+          requestedTime = data['timeToElapse']
+          return requestedTime
+    elif data['periodType'] == 'weeks':
+          requestedTime = data['timeToElapse'] * 7
+          return requestedTime
+    elif data['periodType'] == 'months':
+          requestedTime = data['timeToElapse'] * 30
+          return requestedTime
+
+def estimator(data):
+    reportedCases = data['reportedCases']
+    currentlyInfected = reportedCases * 10
+    severeImpact = currentlyInfected * 5
+    requestedTimeSet = floor(requestedDays(data) / 3)
 
     data = {
-        data : data,
-        impact: {
-          currentlyInfected: currentlyInfected,
-          infectionsByRequestedTime: currentlyInfected * (2 ** requestedTime),
+        'data' : data,
+        'impact': {
+          'currentlyInfected': currentlyInfected,
+          'infectionsByRequestedTime': float(currentlyInfected * 2 ** requestedTimeSet)
         },
-        severeImpact: {
-          severeImpact: severeImpact,
-          currentlyInfected: currentlyInfected,
-          infectionsByRequestedTime : severeImpact * (2 ** requestedTime),
+        'severeImpact': {
+          'currentlyInfected': severeImpact,
+          'infectionsByRequestedTime' : float(severeImpact * 2 ** requestedTimeSet)
         }
     }
 
