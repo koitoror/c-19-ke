43f040215e95aad065240c9c643507abdfbee98b
Daniel Kamar <kamarster@gmail.com>
2020-04-09 14:39:46 +0300

    start building the estimator

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,31 @@
 def estimator(data):
-  return data
+    reportedCases = data.reportedCases
+    currentlyInfected = reportedCases * 10
+    severeImpact = reportedCases * 50
+    # severeImpact = currentlyInfected * 5
+    # severeImpact.currentlyInfected = currentlyInfected
+    requestedTime = data.timeToElapse/3
+    # # infectionsByRequestedTime = currentlyInfected ** requestedTime,
+    # infectionsByRequestedTimeC : currentlyInfected ** requestedTime
+    # infectionsByRequestedTimeS : severeImpact ** requestedTime
+    infectionsByRequestedTime = ''
+
+    
+    impact = {
+        currentlyInfected,
+    }
+
+    data = {
+        data : data,
+        impact: {
+          currentlyInfected: currentlyInfected,
+          infectionsByRequestedTime: currentlyInfected * (2 ** requestedTime),
+        },
+        severeImpact: {
+          severeImpact: severeImpact,
+          currentlyInfected: currentlyInfected,
+          infectionsByRequestedTime : severeImpact * (2 ** requestedTime),
+        }
+    }
+
+    return data
