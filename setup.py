ea59b052ffa9f8b02d12bb7dfae4c3bf10014726
Daniel Kamar <kamarster@gmail.com>
2020-04-11 22:23:17 +0300

    Implement challenge 4

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
 
 
-setup(name="covid19-estimator-py", packages=find_packages())
+setup(name="covid19-estimator-py", packages='requirements.txt')
+# setup(name="covid19-estimator-py", packages=find_packages())
