8da69257b8748842e0498f8ff22662895e150626
Daniel Kamar <kamarster@gmail.com>
2020-04-11 16:49:02 +0300

    Implement functionality for routes

--- manage.py
+++ manage.py
@@ -1,5 +1,8 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, request, jsonify
 from flask_restplus import Resource, Api, Namespace, fields, reqparse
+from dicttoxml import dicttoxml
+
+from src.estimator import estimator
 
 app = Flask(__name__)
 
@@ -19,17 +22,39 @@ class InJson(Resource):
     def get(self):
         return {'hello': 'json'}
     def post(self):
-        return {}
+        # def get_estimation_default():
+        req_data = request.get_json()
+        res = estimator(req_data)
+
+        return jsonify(res)
+        # return {}
 
 @api.route('/api/v1/on-covid-19/xml')
 class InXml(Resource):
     def get(self):
         return {'hello': 'xml'}
+    def post(self):
+        arr = []
+        req_data = request.get_json()
+        res = estimator(req_data)
+        dic = {'response': res}
+        arr.append(dic)
+
+        xml = dicttoxml(arr)
+        # xml = dicttoxml(dic, custom_root='test', attr_type=False)
+        # xml = dicttoxml(arr, custom_root='test', attr_type=False)
+
+        # return jsonify(res)
+        return xml
+        # return {}
+
 
 @api.route('/api/v1/on-covid-19/logs')
 class Logs(Resource):
     def get(self):
         return {'hello': 'logs'}
+    def post(self):
+        return {}
 
 if __name__ == '__main__':
     app.run(debug=True)
