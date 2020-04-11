efb9e8f14854b0b8966d736051ec4d8d8de1219a
Daniel Kamar <kamarster@gmail.com>
2020-04-11 22:47:14 +0300

    Add dependencies

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, find_packages
 
 
-setup(name="covid19-estimator-py", packages='requirements.txt')
-# setup(name="covid19-estimator-py", packages=find_packages())
+# setup(name="covid19-estimator-py", packages='requirements.txt')
+setup(
+    name="covid19-estimator-py", 
+    packages=find_packages(),  
+    install_requires=[
+        'flask', 
+        'flask-restplus',
+        'dicttoxml',
+        'werkzeug==0.16.1'
+        ])
