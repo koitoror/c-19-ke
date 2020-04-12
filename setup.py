0ca324acbd5e56fa80c25a0455b46efa00045df1
Daniel Kamar <kamarster@gmail.com>
2020-04-12 03:54:25 +0300

    Implementation finalizaition for challenge

--- setup.py
+++ setup.py
@@ -1,24 +1,10 @@
 from setuptools import setup, find_packages
 
 
-# setup(name="covid19-estimator-py", packages='requirements.txt')
-# setup(
-#     name="covid19-estimator-py", 
-#     packages=find_packages(),  
-#     install_requires=[
-#         'flask', 
-#         'flask-restplus',
-#         'dicttoxml',
-#         'werkzeug==0.16.1'
-#         ])
-
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 with open("requirements.txt", "r") as f:
-
-    # requirements = fh.readlines()
     requirements = f.read().splitlines()
 
 
@@ -32,7 +18,6 @@ setup(
     long_description_content_type="text/markdown",
     url="https://cov-19-ke.herokuapp.com",
     packages=find_packages(),
-    # install_requires=[req for req in requirements if req[:2] != "# "],
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
